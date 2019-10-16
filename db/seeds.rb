0b256df739ee68ac0bf0a2bc09aa5277a22d7c68
Mustafa Muse <umpp101@icloud.com>
2019-10-16 13:17:33 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+first_song = Song.create(name: "Problems with you")
+second_song = Song.create(name: "Time")
+third_song = Song.create(name: "Kontrol")
+
+first_artist = Artist.create(name: "Chris Brown", bio: "skadnasdn askdlaksmlda asdklna")
+second_artist = Artist.create(name: "Lil Baby", bio: "eenasdn askdlaksmasdadslda asdklna")
+third_artist = Artist.create(name: "Maleek Berry", bio: "jsfksjf jemfem")
+
+first_genre = Genre.create(name: "R&B")
+second_genre = Genre.create(name: "Hip Hop")
+third_genre = Genre.create(name: "Pop")
